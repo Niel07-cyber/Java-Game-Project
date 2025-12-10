694cf7794995c26660e24d5d2ba9e6f3319d0b4a
mikekane007 <mikekanex24@gmail.com>
2025-12-11 00:53:57 +0100

    add MonteCarloRNG for color

---
+++ src/polymorphicSimulation/utils/MonteCarloRNG.java
@@ -0,0 +1,24 @@
+package src.polymorphicSimulation.utils;
+
+import java.util.Random;
+
+public class MonteCarloRNG {
+    private static final Random RANDOM = new Random();
+
+    public static int getInt(int min, int max) {
+        if (min > max) {
+            throw new IllegalArgumentException("Max must be greater than or equal to min");
+        }
+        return RANDOM.nextInt((max - min) + 1) + min;
+    }
+
+    public static boolean chance(double probability) {
+        return RANDOM.nextDouble() < probability;
+    }
+
+    public static <T> T getItem(T[] array) {
+        if (array == null || array.length == 0)
+            return null;
+        return array[RANDOM.nextInt(array.length)];
+    }
+}
