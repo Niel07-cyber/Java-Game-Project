9c892f10557332da7ab20f45a6255c3d938f136f
mikekane007 <mikekanex24@gmail.com>
2025-12-11 01:15:12 +0100

    implement factory for centralising agent and master creation

---
+++ src/polymorphicSimulation/utils/SingletonMasterFactory.java
@@ -0,0 +1,51 @@
+package src.polymorphicSimulation.utils;
+
+import src.polymorphicSimulation.agents.*;
+
+public class SingletonMasterFactory {
+
+    public static Agent createAgent(Species species, int x, int y, int maxEnergy) {
+        switch (species) {
+            case BOWSER:
+                return new Bowser(x, y, maxEnergy);
+            case KING_BOO:
+                return new KingBoo(x, y, maxEnergy);
+            case LUIGI:
+                return new Luigi(x, y, maxEnergy);
+            case MARIO:
+                return new Mario(x, y, maxEnergy);
+            default:
+                throw new IllegalArgumentException("Unknown species: " + species);
+        }
+    }
+
+    public static Master getMaster(Species species, int x, int y) {
+        switch (species) {
+            case BOWSER:
+                return MasterBowser.getInstance(x, y);
+            case KING_BOO:
+                return MasterKingBoo.getInstance(x, y);
+            case LUIGI:
+                return MasterLuigi.getInstance(x, y);
+            case MARIO:
+                return MasterMario.getInstance(x, y);
+            default:
+                throw new IllegalArgumentException("Unknown Master species");
+        }
+    }
+
+    public static Master getMaster(Species species) {
+        switch (species) {
+            case BOWSER:
+                return MasterBowser.getInstance();
+            case KING_BOO:
+                return MasterKingBoo.getInstance();
+            case LUIGI:
+                return MasterLuigi.getInstance();
+            case MARIO:
+                return MasterMario.getInstance();
+            default:
+                throw new IllegalArgumentException("Unknown Master species");
+        }
+    }
+}
